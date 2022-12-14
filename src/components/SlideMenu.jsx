99fec0739b4c23f0bb5977129f3ca33462c11b68
yterasaka <yuki.terasaka@gmail.com>
2022-12-14 14:56:52 +0100

    Add Icons in SlideMenu

--- src/components/SlideMenu.jsx
+++ src/components/SlideMenu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import styled from "styled-components";
-
+import { BsStickies, BsStar, BsTrash } from "react-icons/bs";
 
 const Link = styled.a`
   color: #373a47;
@@ -13,64 +13,64 @@ const Link = styled.a`
 
 const styles = {
   bmBurgerButton: {
-    position: 'fixed',
-      width: '30px',
-      height: '25px',
-      left: '30px',
-      top: '22px'
+    position: "fixed",
+    width: "30px",
+    height: "25px",
+    left: "30px",
+    top: "22px",
   },
   bmBurgerBars: {
-    background: '#373a47'
+    background: "#373a47",
   },
   bmBurgerBarsHover: {
-    background: '#a90000'
+    background: "#a90000",
   },
   bmCrossButton: {
-    height: '24px',
-    width: '24px'
+    height: "24px",
+    width: "24px",
   },
   bmCross: {
-    background: '#bdc3c7'
+    background: "#bdc3c7",
   },
   bmMenuWrap: {
-    position: 'fixed',
-    height: '100%',
-    width: '250px',
-    top: '0',
-    left: '0'
+    position: "fixed",
+    height: "100%",
+    width: "250px",
+    top: "0",
+    left: "0",
   },
   bmMenu: {
-    background: '#fff',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em',
+    background: "#fff",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
   },
   bmMorphShape: {
-    fill: '#373a47'
+    fill: "#373a47",
   },
   bmItemList: {
-    color: '#b8b7ad',
+    color: "#b8b7ad",
   },
   bmItem: {
-   
+    marginBottom: "1rem",
+    marginLeft: "1rem"
   },
   bmOverlay: {
-    top: '0',
-    background: 'rgba(0, 0, 0, 0.3)'
-  }
-}
-
+    top: "0",
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
 
 const SlideMenu = () => {
   return (
-    <Menu styles={ styles }>
+    <Menu styles={styles}>
       <Link id="all" href="/">
-        All Notes
+        <BsStickies /> All Notes
       </Link>
       <Link id="star" href="/">
-        Star
+        <BsStar /> Star
       </Link>
       <Link id="trash" href="/">
-        Trash
+        <BsTrash /> Trash
       </Link>
     </Menu>
   );
