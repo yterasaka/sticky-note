cc51867c1915332a18b9c60f21bd3b6a45279347
yterasaka <yuki.terasaka@gmail.com>
2023-01-18 13:11:46 +0100

    Add GitHub and LinkdIn links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { BsStickies, BsStar, BsTrash } from "react-icons/bs";
+import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
 import logo from "../image/logo_transparent.png";
 // import SlideMenu from "./SlideMenu";
 
@@ -22,9 +23,13 @@ const HeaderWrapper = styled.header`
 const Logo = styled.img`
   margin: 0 auto;
   padding: 0.2rem;
+  position: fixed;
+  height: 4rem;
+  right: 50%;
+  left: 50%;
 `;
 const FilterMenu = styled.div`
-  margin: auto 5px;
+  margin: auto 10px;
 `;
 
 const FilterButton = styled.button`
@@ -56,6 +61,20 @@ const FilterButtonTrash = styled(FilterButton)`
   background-color: ${({ filter }) =>
     filter === "trash" ? "#FFB6C1" : "#f5f5f5"};
 `;
+
+const LinkButtonMenu = styled.div`
+  margin: auto 35px;
+`
+
+const LinkButton = styled.a`
+  font-size: 1.4rem;
+  color: #36393A;
+  margin-left: 20px;
+  &:hover {
+    opacity: 0.7;
+    cursor: pointer;
+  }
+`
 
 export const Header = ({ filter, setFilter }) => {
   // const [isOpen, setIsOpen] = useState();
@@ -93,10 +112,14 @@ export const Header = ({ filter, setFilter }) => {
         </FilterButtonTrash>
       </FilterMenu>
       <Logo src={logo} alt="image" />
-      <ul>
-        <li>GitHub</li>
-        <li>LinkdIn</li>
-      </ul>
+      <LinkButtonMenu>
+        <LinkButton hrem="/" >
+          <AiFillGithub />
+        </LinkButton>
+        <LinkButton hrem="/">
+          <AiFillLinkedin />
+        </LinkButton>
+      </LinkButtonMenu>
     </HeaderWrapper>
   );
 };
